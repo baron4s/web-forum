b28618394eac6525a80f41546e9412366bc4eb83
Roni Saputra <saputraroni271000@gmail.com>
2023-09-22 00:29:58 +0700

    make test error

--- src/states/auth/reducer.js
+++ src/states/auth/reducer.js
@@ -2,7 +2,7 @@ import { ActionType } from './action';
 
 function authUserReducer(authUser = null, action = {}) {
   switch (action.type) {
-    case ActionType.SET_AUTH_USER:
+    case ActionType.SET_AUTH_USSER:
       return action.payload.authUser;
     case ActionType.UNSET_AUTH_USER:
       return action.payload.authUser;
