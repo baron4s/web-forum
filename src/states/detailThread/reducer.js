b19a279d74a6d7ec7ad6297430c1b5526c5a6d97
Roni Saputra <saputraroni271000@gmail.com>
2023-09-22 01:18:47 +0700

    make test pass

--- src/states/detailThread/reducer.js
+++ src/states/detailThread/reducer.js
@@ -26,7 +26,7 @@ function detailThreadReducer(detailThread = null, action = {}) {
           (id) => id !== action.payload.userId,
         ),
       };
-    case ActionType.UP_VOTE_THREAD_DETAIL_COMMENT:
+    case ActionType.UP_VOTE_THREAD_DETAIL_COMMENTS:
       return {
         ...detailThread,
         comments: detailThread.comments.map((comment) => {
